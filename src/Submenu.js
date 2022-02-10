3b665edf6ad4a8288a304e3379b2cfd23f506440
Redon <redonkurtaj123@gmail.com>
2022-02-10 20:03:34 +0100

    Css

--- src/Submenu.js
+++ src/Submenu.js
@@ -4,14 +4,20 @@ const Submenu =()=> {
     const{isSubmenuOpen, location: {center, bottom}, page: {page, links}} = useGlobalContext()
     return (
       <aside className={`${isSubmenuOpen? "submenu show":"submenu"}`} style={{top: bottom, left:center}}>
-        <section>
-          <h4>{page}</h4>
-          <div className="submenu-center">
-            <p>Submenu</p>
-            <p>Submenu</p>
-            <p>Submenu</p>
+        <article>
+          <h4 className="meny-desktop">{page}</h4>
+          <div className="sidebar-sublinks">
+          {links.map((link, index) => {
+   const { url, icon, label } = link;
+   return (
+     <a href={url}>
+       <span className="sublinks-icon">{icon} </span>
+       {label}
+     </a>
+   );
+ })}
           </div>
-        </section>
+        </article>
       </aside>
     );
 }
